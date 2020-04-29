f8b379141c6acb1b03536f20d4b5c6718b4d2a4b
Ivan <st.onlyemail@gmail.com>
2020-04-29 19:48:28 +0300

    Switch mechanism
    - Switch mechanism added
    - Removed unused menu types
    - class GameMenu created
    - bug with pause menu fixed

---
+++ src/menugraph.cpp
@@ -0,0 +1,6 @@
+#include "menugraph.h"
+
+MenuGraph::MenuGraph()
+{
+
+}
---
+++ src/menugraph.h
@@ -0,0 +1,11 @@
+#ifndef MENUGRAPH_H
+#define MENUGRAPH_H
+
+
+class MenuGraph
+{
+public:
+    MenuGraph();
+};
+
+#endif // MENUGRAPH_H
